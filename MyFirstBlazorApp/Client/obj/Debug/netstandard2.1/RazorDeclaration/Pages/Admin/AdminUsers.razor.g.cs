b94be6280d00be401e46d6837049d9a7d91269b0
Ylvin Pérez <ylvinzk@users.noreply.github.com>
2021-02-22 18:06:05 -0400

    One way binding

--- MyFirstBlazorApp/Client/obj/Debug/netstandard2.1/RazorDeclaration/Pages/Admin/AdminUsers.razor.g.cs
+++ MyFirstBlazorApp/Client/obj/Debug/netstandard2.1/RazorDeclaration/Pages/Admin/AdminUsers.razor.g.cs
@@ -13,69 +13,76 @@ namespace MyFirstBlazorApp.Client.Pages.Admin
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\yperez\source\playground\MyFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
+#line 1 "C:\Users\yperez\Workspace\source\repos\MyiFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\yperez\source\playground\MyFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
+#line 2 "C:\Users\yperez\Workspace\source\repos\MyiFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\yperez\source\playground\MyFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
+#line 3 "C:\Users\yperez\Workspace\source\repos\MyiFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\yperez\source\playground\MyFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
+#line 4 "C:\Users\yperez\Workspace\source\repos\MyiFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\yperez\source\playground\MyFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
+#line 5 "C:\Users\yperez\Workspace\source\repos\MyiFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\yperez\source\playground\MyFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
+#line 6 "C:\Users\yperez\Workspace\source\repos\MyiFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\yperez\source\playground\MyFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
+#line 7 "C:\Users\yperez\Workspace\source\repos\MyiFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\yperez\source\playground\MyFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
+#line 8 "C:\Users\yperez\Workspace\source\repos\MyiFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
 using MyFirstBlazorApp.Client;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\yperez\source\playground\MyFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
+#line 9 "C:\Users\yperez\Workspace\source\repos\MyiFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
 using MyFirstBlazorApp.Client.Shared;
 
 #line default
 #line hidden
 #nullable disable
-    [Microsoft.AspNetCore.Components.LayoutAttribute(typeof(MainLayout))]
+#nullable restore
+#line 10 "C:\Users\yperez\Workspace\source\repos\MyiFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
+using MyFirstBlazorApp.Client.Components;
+
+#line default
+#line hidden
+#nullable disable
+    [Microsoft.AspNetCore.Components.LayoutAttribute(typeof(AdminLayout))]
     [Microsoft.AspNetCore.Components.RouteAttribute("/admin/users")]
     public partial class AdminUsers : Microsoft.AspNetCore.Components.ComponentBase
     {
