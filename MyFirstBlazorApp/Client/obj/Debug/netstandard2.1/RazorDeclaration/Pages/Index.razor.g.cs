dfd9509fbb5679d963a28fcda6aa42dcb888de66
Ylvin Perez <yperez@synergiescorp.com>
2021-02-23 11:49:31 -0400

    Literals, expressions and directives.
    Signed-off-by: Ylvin Pérez <yperez@synergiescorp.com>

--- MyFirstBlazorApp/Client/obj/Debug/netstandard2.1/RazorDeclaration/Pages/Index.razor.g.cs
+++ MyFirstBlazorApp/Client/obj/Debug/netstandard2.1/RazorDeclaration/Pages/Index.razor.g.cs
@@ -13,71 +13,78 @@ namespace MyFirstBlazorApp.Client.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\yperez\Workspace\source\repos\MyiFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
+#line 1 "C:\workspace\source\repos\playground\MyFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\yperez\Workspace\source\repos\MyiFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
+#line 2 "C:\workspace\source\repos\playground\MyFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\yperez\Workspace\source\repos\MyiFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
+#line 3 "C:\workspace\source\repos\playground\MyFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\yperez\Workspace\source\repos\MyiFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
+#line 4 "C:\workspace\source\repos\playground\MyFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\yperez\Workspace\source\repos\MyiFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
+#line 5 "C:\workspace\source\repos\playground\MyFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\yperez\Workspace\source\repos\MyiFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
+#line 6 "C:\workspace\source\repos\playground\MyFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\yperez\Workspace\source\repos\MyiFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
+#line 7 "C:\workspace\source\repos\playground\MyFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\yperez\Workspace\source\repos\MyiFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
+#line 8 "C:\workspace\source\repos\playground\MyFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
 using MyFirstBlazorApp.Client;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\yperez\Workspace\source\repos\MyiFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
+#line 9 "C:\workspace\source\repos\playground\MyFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
 using MyFirstBlazorApp.Client.Shared;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "C:\Users\yperez\Workspace\source\repos\MyiFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
+#line 10 "C:\workspace\source\repos\playground\MyFirstBlazorApp\MyFirstBlazorApp\Client\_Imports.razor"
 using MyFirstBlazorApp.Client.Components;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 2 "C:\workspace\source\repos\playground\MyFirstBlazorApp\MyFirstBlazorApp\Client\Pages\Index.razor"
+using System.Diagnostics;
 
 #line default
 #line hidden
@@ -90,6 +97,32 @@ using MyFirstBlazorApp.Client.Components;
         {
         }
         #pragma warning restore 1998
+#nullable restore
+#line 24 "C:\workspace\source\repos\playground\MyFirstBlazorApp\MyFirstBlazorApp\Client\Pages\Index.razor"
+ 
+    int lastMultipleOfThree;
+
+    private void UpdateLastMultipleOfThreeValue(int value)
+    {
+        lastMultipleOfThree = value;
+    }
+
+    private static void TopLevelClicked()
+    {
+        Debug.WriteLine("Top level clicked");
+    }
+    private static void SecondLevelClicked()
+    {
+        Debug.WriteLine("Second level clicked");
+    }
+    private static void ThirdLevelClicked()
+    {
+        Debug.WriteLine("Third level clicked");
+    }
+
+#line default
+#line hidden
+#nullable disable
     }
 }
 #pragma warning restore 1591
